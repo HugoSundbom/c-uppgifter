f196125ec1d7e5920ead1e4baf5df95499263e67
Hugo Sundbom <hugo.sundbom@elev.ga.ntig.se>
2022-08-31 09:35:12 +0200

    Uppgift 1-20

--- csharp-övningar/csharp-övningar/Program.cs
+++ csharp-övningar/csharp-övningar/Program.cs
@@ -48,12 +48,84 @@ namespace csharp_övningar
             Console.ReadLine();
             Console.WriteLine("Adios");
             */
-            // UPPGIFT 10
+            /* UPPGIFT 10
             Console.WriteLine("Nu ska jag berätta en historia om dig.\nVad heter du?");
             string name = Console.ReadLine();
             Console.WriteLine("Vad jobbar du med?");
             string job = Console.ReadLine();
-            Console.WriteLine("Det var en gång " + name + "som jobbade som " + job + ". " + name + "föll ut för ett stup och dog.");
+            Console.WriteLine("Det var en gång " + name + " som jobbade som " + job + ". " + name + " föll ut för ett stup och dog.");
+            */
+            /*UPPGIFT 11
+            int days = 200;
+            double hours = 3.5;
+            string coding = "It sure is!";
+            Console.WriteLine("Days to summer:");
+            Console.WriteLine(days);
+            Console.WriteLine("Hours to lunch:");
+            Console.WriteLine(hours);
+            Console.WriteLine("Coding is fun:");
+            Console.WriteLine(coding);
+            */
+            /*UPPGIFT 12
+            Console.WriteLine("skriv ett heltal");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input);
+            */
+            /*UPPGIFT 13
+            Console.WriteLine("Skriv ett decimaltal");
+            double input = Convert.ToDouble(Console.ReadLine());
+            */
+            /*UPPGIFT 14
+            Console.WriteLine("GIVE ME THE TRUTH");
+            bool input = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine(input);
+            */
+            /*UPPGIFT 15
+            Console.WriteLine("GE MIG EN STRING");
+            string str = Console.ReadLine();
+            Console.WriteLine("GE MIG EN INT");
+            int intiger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("GE MIG EN DOUBLE");
+            double dub = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("GE MIG EN BOOLEAN");
+            bool boo = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("STRING " + str + "\nINT " + intiger + "\nDOUBLE " + dub + "\nBOOLEAN " + boo);
+            */
+            /*UPPGIFT 16
+            Console.WriteLine("HUR MÅNGA DAGAR?");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int sek = input * 86400;
+            Console.WriteLine(sek);
+            */
+            /*UPPGIFT 17
+            Console.WriteLine("SKRIV 2 INTS");
+            int one = Convert.ToInt32(Console.ReadLine());
+            int two = Convert.ToInt32(Console.ReadLine());
+            int add = one + two;
+            Console.WriteLine(add);
+            */
+            /*UPPGIFT 18
+            Console.WriteLine("SKRIV 3 INTS");
+            int one = Convert.ToInt32(Console.ReadLine());
+            int two = Convert.ToInt32(Console.ReadLine());
+            int three = Convert.ToInt32(Console.ReadLine());
+            int add = one + two + three;
+            Console.WriteLine(add);
+            */
+            /*UPPGIFT 19
+            Console.WriteLine("SKRIV 2 INTS");
+            int one = Convert.ToInt32(Console.ReadLine());
+            int two = Convert.ToInt32(Console.ReadLine());
+            int add = one + two;
+            Console.WriteLine(one + " + " + two + " = " + add);
+            */
+            /*UPPGIFT 20
+            Console.WriteLine("SKRIV 2 INTS");
+            int one = Convert.ToInt32(Console.ReadLine());
+            int two = Convert.ToInt32(Console.ReadLine());
+            int mult = one * two;
+            Console.WriteLine(mult);
+            */
         }
     }
 }
